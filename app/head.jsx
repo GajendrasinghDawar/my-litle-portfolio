1b9e16d5fc99db5ec0025bfe771b50bfe1cdbc14
GajendrasinghDawar <babyfirefox786@gmail.com>
2023-01-06 22:45:22 +0530

    head

--- app/head.jsx
+++ app/head.jsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>Gajendrasingh Dawar</title>
+      <title>Gajendrasingh Dawar👻</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Gajendrasingh dawar's litle website." />
       <link rel="icon" href="/favicon.ico" />
